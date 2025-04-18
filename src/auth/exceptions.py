a442700a4616ee7a554b54f3cb500df842e72587
lloid <eraliev.adilet006@gmail.com>
2025-04-18 14:05:06 -0500

    feat(auth.exceptions): add custom exceptions

---
+++ src/auth/exceptions.py
@@ -0,0 +1,23 @@
+from fastapi import HTTPException
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+
+class AuthException(HTTPException):
+    def __init__(self, detail: str):
+        super().__init__(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail=detail
+        )
+
+
+class InvalidSignatureException(AuthException):
+    def __init__(self):
+        super().__init__(
+            detail='Invalid token signature error',
+        )
+
+class ExpiredTokenSignatureException(AuthException):
+    def __init__(self):
+        super().__init__(
+            detail='Token expired',
+        )
