47a86dfe72fb91851fb3bed7abfda8ef4f4b0298
lloid <eraliev.adilet006@gmail.com>
2025-04-18 15:30:23 -0500

    feat(users.models): add models of user. Also added schemas Update, Create user

---
+++ src/users/models.py
@@ -0,0 +1,28 @@
+import datetime
+from typing import Optional
+
+from sqlmodel import SQLModel, Field
+
+
+class BaseUser(SQLModel):
+    first_name: str = Field(max_length=100)
+    last_name: str = Field(max_length=100)
+    avatar_url: Optional[str] = Field(nullable=True)
+
+class User(BaseUser, table = True):
+    id: Optional[int] = Field(primary_key=True, nullable=False, index=True)
+    password: str = Field(nullable=False)
+    created_at: datetime.date
+
+class UserCreate(BaseUser):
+    password: str
+
+class UserRead(BaseUser):
+    id: int
+    created_at: datetime.date
+
+class UserUpdate(BaseUser):
+    pass
+
+
+
