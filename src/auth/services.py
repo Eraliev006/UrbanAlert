7aca2aa68638a3c89fa2a2658f32fb1d3f72fc2a
lloid <eraliev.adilet006@gmail.com>
2025-04-19 05:27:45 -0500

    feat(auth.services): add a register_user and login_user function

---
+++ src/auth/services.py
@@ -0,0 +1,62 @@
+from typing import Optional
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.auth import EmailAlreadyExists, hash_password, LoginUserRead, LoginUserOutput, UserWithEmailNotFound, \
+    verify_password, PasswordIsIncorrect, TokenPairs, UserNotVerifyEmail
+from src.auth.utils import get_pairs_token
+from src.users import UserCreate, User
+from src.users import get_user_by_email, create_user
+
+
+async def register_user(db_session: AsyncSession, user:UserCreate):
+    exists_user: Optional[User] = await get_user_by_email(db_session,str(user.email))
+
+    if exists_user:
+        raise EmailAlreadyExists(str(user.email))
+
+    hashed_password: str = hash_password(user.password.encode())
+    user = UserCreate(
+        **user.model_dump(exclude={'password'}),
+        password = hashed_password
+    )
+
+    created_user: Optional[User] = await create_user(
+        db_session,
+        user
+    )
+
+    # ------------------- SEND EMAIL VERIFY LINK  WITH OTP CODE --------------------
+    return created_user
+
+
+async def login_user(db_session: AsyncSession,login_data: LoginUserRead) -> LoginUserOutput:
+    exists_user: Optional[User] = await get_user_by_email(db_session, str(login_data.email))
+
+    if not exists_user:
+        raise UserWithEmailNotFound(str(login_data.email))
+
+    if not exists_user.is_verified:
+        raise UserNotVerifyEmail
+
+    if not verify_password(
+        plain_pass=login_data.password.encode(),
+        hashed_password=exists_user.password.encode()
+    ):
+        raise PasswordIsIncorrect
+
+    payload = {
+        'sub': 'user',
+        'user_id': exists_user.id,
+        'is_verified': exists_user.is_verified
+    }
+    tokens: TokenPairs = get_pairs_token(payload)
+
+    return LoginUserOutput(
+        access_token = tokens.access_token,
+        refresh_token = tokens.refresh_token,
+        token_type = 'bearer'
+    )
+
+
+
