929f67ccc7508ede46603de2347d9993e30acafa
lloid <eraliev.adilet006@gmail.com>
2025-04-19 15:29:31 -0500

    refactor(users.__init__): refator crud to .crud

--- src/users/__init__.py
+++ src/users/__init__.py
@@ -1,3 +1,3 @@
 from .models import User, UserCreate, UserRead, UserUpdate
 from .exceptions import UserWithIdNotFound
-from crud import create_user, get_user_by_email, get_user_by_id, update_user_by_id, delete_user_by_id
+from .crud import create_user, get_user_by_email, get_user_by_id, update_user_by_id, delete_user_by_id
