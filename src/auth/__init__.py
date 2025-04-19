5e476ef2cc1a434769694cae2031c10e61cbb721
lloid <eraliev.adilet006@gmail.com>
2025-04-19 05:18:54 -0500

    add import of PasswordIsIncorrect exception

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,3 +1,3 @@
 from .schemas import LoginUserOutput, LoginUserRead, TokenPairs
-from .exceptions import InvalidSignatureException, EmailAlreadyExists, UserWithEmailNotFound
+from .exceptions import InvalidSignatureException, EmailAlreadyExists, UserWithEmailNotFound, PasswordIsIncorrect
 from .utils import hash_password, verify_password
