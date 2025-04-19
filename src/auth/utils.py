e09fd0a299d53f0e256d79328013ac5367b3f768
lloid <eraliev.adilet006@gmail.com>
2025-04-19 00:46:24 -0500

    refactor(auth.utils): moved create_token to tokens directory. Now we just have get_pairs_token to get pairs of access and refresh tokens

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,6 +1,3 @@
-import datetime
-import enum
-from datetime import timezone, timedelta
 from typing import Any
 
 import jwt
@@ -8,45 +5,14 @@ from jwt import InvalidSignatureError, ExpiredSignatureError
 
 from src.auth import TokenPairs
 from src.auth.exceptions import ExpiredTokenSignatureException, InvalidSignatureException
+from src.auth.tokens.access import AccessTokenCreator
+from src.auth.tokens.refresh import RefreshTokenCreator
 from src.core import settings
 
-class TokenType(enum.Enum):
-    refresh = 'refresh'
-    access = 'access'
 
-def _create_token(
-        payload: dict[str, Any],
-        secret_key: str,
-        algorithm: str,
-        token_type: TokenType,
-        access_expire_in_minutes: int = settings.jwt.access_expires_in_minutes,
-        refresh_expire_in_minutes: int = settings.jwt.refresh_expires_in_minutes,
-) -> str:
-    expire_minutes = (
-        refresh_expire_in_minutes if token_type == TokenType.refresh else access_expire_in_minutes
-    )
-
-    exp = datetime.datetime.now(tz=timezone.utc) + timedelta(minutes=expire_minutes)
-    return jwt.encode(
-        payload={
-            **payload,
-            'type': token_type.value,
-            'exp': exp
-        },
-        key=secret_key,
-        algorithm=algorithm
-    )
-
-def create_tokens(
-        payload: dict[str, Any],
-        secret_key: str = settings.jwt.secret_key,
-        algorithm: str = settings.jwt.algorithm,
-        access_expire_in_minutes: int = settings.jwt.access_expires_in_minutes,
-        refresh_expire_in_minutes: int = settings.jwt.refresh_expires_in_minutes,
-) -> TokenPairs:
-
-    access_token = _create_token(payload, secret_key, algorithm, TokenType.access, access_expire_in_minutes)
-    refresh_token = _create_token(payload, secret_key, algorithm, TokenType.refresh, refresh_expire_in_minutes)
+def get_pairs_token(payload: dict[str, Any]) -> TokenPairs:
+    access_token = AccessTokenCreator().generate(payload)
+    refresh_token = RefreshTokenCreator().generate(payload)
 
     return TokenPairs(
         access_token = access_token,
