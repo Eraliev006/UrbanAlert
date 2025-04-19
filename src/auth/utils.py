7dc9c5abfa7c48dfe2b73887005c1d7e951769c4
lloid <eraliev.adilet006@gmail.com>
2025-04-19 04:41:22 -0500

    feat(auth.utils): add verify_password function

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -42,3 +42,9 @@ def hash_password(password: bytes) -> str:
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
     return pwd_context.hash(password)
+
+def verify_password(plain_pass: bytes, hashed_password: bytes) -> bool:
+    """Verify password"""
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    return pwd_context.verify(plain_pass, hashed_password)
