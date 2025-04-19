c4f6c0ebe1e04238c44c686a30f66465ae6450c4
lloid <eraliev.adilet006@gmail.com>
2025-04-19 05:25:09 -0500

    add import of UserNotVerifyEmail exception

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,3 +1,4 @@
 from .schemas import LoginUserOutput, LoginUserRead, TokenPairs
-from .exceptions import InvalidSignatureException, EmailAlreadyExists, UserWithEmailNotFound, PasswordIsIncorrect
+from .exceptions import (InvalidSignatureException, EmailAlreadyExists,
+                         UserWithEmailNotFound, PasswordIsIncorrect, UserNotVerifyEmail)
 from .utils import hash_password, verify_password
