31d735f662f4bae19e9f29090c1bc28546447da5
lloid <eraliev.adilet006@gmail.com>
2025-04-19 05:29:17 -0500

    add import of register and login user function

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -2,3 +2,4 @@ from .schemas import LoginUserOutput, LoginUserRead, TokenPairs
 from .exceptions import (InvalidSignatureException, EmailAlreadyExists,
                          UserWithEmailNotFound, PasswordIsIncorrect, UserNotVerifyEmail)
 from .utils import hash_password, verify_password
+from .services import register_user, login_user
