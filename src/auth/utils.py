43f6f6e1a7532e4e7fb28d6048599b66fd49181f
lloid <eraliev.adilet006@gmail.com>
2025-04-19 04:59:58 -0500

    feat(auth.utils): add function to generate OTP code

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -49,3 +49,6 @@ def verify_password(plain_pass: bytes, hashed_password: bytes) -> bool:
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
     return pwd_context.verify(plain_pass, hashed_password)
+
+def generate_otp_code(length: int = 6) -> str:
+    return ''.join([str(random.randint(0, 9)) for _ in range(length)])
