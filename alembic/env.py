1686c4f9690ce240d28e4ab12f7a1a2257de76ce
lloid <eraliev.adilet006@gmail.com>
2025-04-20 12:00:38 -0500

    changed target_metadata to SQLModel.metadata and set sqlalchemy.url to my db url

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,9 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+from sqlmodel import SQLModel
+
+from src.core import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,12 +23,13 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+config.set_main_option('sqlalchemy.url', settings.database.get_url())
 
 
 def run_migrations_offline() -> None:
