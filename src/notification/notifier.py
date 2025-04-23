520c1342e50f6102a805225ad28b51cb2671e543
= <=>
2025-04-23 20:33:18 +0600

    refactor[notification.notifier]: add param otp_code: int

--- src/notification/notifier.py
+++ src/notification/notifier.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class Notifier(ABC):
     @abstractmethod
-    def notify(self, to_user: str):
+    def notify(self, to_user: str, otp_code: int):
         raise NotImplemented
