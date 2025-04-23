6132f2bd04b079af1ddbee20cd63cf27add54513
= <=>
2025-04-23 20:34:51 +0600

    feat[notification.email_notifier]: Implement OTP code email sending

--- src/notification/email_notifier.py
+++ src/notification/email_notifier.py
@@ -1,6 +1,13 @@
+from email.message import EmailMessage
+
+from src.core import settings
 from src.notification import Notifier
 
 
 class EmailNotifier(Notifier):
-    def notify(self):
-        pass
+
+    def notify(self, to_user: str, otp_code: int):
+        message = EmailMessage()
+        message['From'] = settings.smtp.user_email
+        message['To'] = to_user
+        message['Subject'] = f'Your One-Time Password — {otp_code}'
