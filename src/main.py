b76db15ebe90bc2b488716817dc6d974b1837069
= <=>
2025-04-23 21:55:21 +0600

    feat[main]: include user router

--- src/main.py
+++ src/main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 
 from src.core import settings
 from auth.routes import router as auth_router
+from users.routes import router as user_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(user_router)
 
 if __name__ == "__main__":
     uvicorn.run(
