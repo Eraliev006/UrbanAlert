5c403ec5c5353418b13475b75afe14d8357d3f79
= <=>
2025-04-25 23:37:37 +0600

    feat[main]: add lifespan

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 import uvicorn
 from fastapi import FastAPI
 
-from src.core import settings
+from src.core import settings, redis_client, database_helper
 from auth.routes import router as auth_router
 from users.routes import router as user_router
 
-app = FastAPI()
+
+
+async def lifespan(app: FastAPI):
+    await redis_client.connect()
+
+    yield
+
+    await redis_client.close()
+    await database_helper.dispose()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_router)
 app.include_router(user_router)
