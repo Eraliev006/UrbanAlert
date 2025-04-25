2a8993d53dad0fb92d091d9e32578eca523b2b22
= <=>
2025-04-25 23:38:55 +0600

    feat[core.redis]: add redis client to connect, save, get, and delete

--- src/core/redis_client.py
+++ src/core/redis_client.py
@@ -1,10 +1,45 @@
+import redis.asyncio as redis
+
+from src.core import settings
+
 
 class RedisClient:
 
-    def __init__(self):
-        pass
+    def __init__(self, host: str = 'localhost', port: int = 6379, db: int = 0, decode_responses: bool = True):
+        self._client = None
+        self.port = port
+        self.host = host
+        self.db = db
+        self.decode_responses = decode_responses
 
-    def save_data(self):
-        pass
+    async def connect(self):
+        self._client = redis.Redis(
+            port = self.port,
+            host = self.host,
+            db = self.db,
+            decode_responses = self.decode_responses
+        )
 
-    def
+    async def close(self):
+        await self._client.close()
+
+    def get_client(self) -> redis.Redis:
+        if not self._client:
+            raise RuntimeError('Redis is not connected')
+        return self._client
+
+    async def set(self, key: str, value: str | int | dict | list | set, ex: int = None):
+        await self.get_client().set(name=key, value=value, ex=ex)
+
+    async def get(self, key: str):
+        return self.get_client().get(name=key)
+
+    async def delete(self, key: str):
+        await self.get_client().delete(key)
+
+redis_client = RedisClient(
+    port = settings.redis.port,
+    host = settings.redis.host,
+    db=0,
+    decode_responses=True
+)
