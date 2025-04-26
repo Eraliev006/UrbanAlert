c374bd5bb83e1122d8e33111d6892ccca778e88a
= <=>
2025-04-26 14:07:37 +0600

    add imports

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,5 +1,5 @@
-from .schemas import LoginUserOutput, LoginUserRead, TokenPairs
+from .schemas import LoginUserOutput, LoginUserRead, TokenPairs, VerifyEmailSchema
 from .exceptions import (InvalidSignatureException, EmailAlreadyExists,
                          UserWithEmailNotFound, PasswordIsIncorrect, UserNotVerifyEmail)
 from .utils import hash_password, verify_password
-from .services import register_user, login_user
+from .services import register_user, login_user, verify_user_by_otp_code
--- src/users/__init__.py
+++ src/users/__init__.py
@@ -1,3 +1,3 @@
 from .models import User, UserCreate, UserRead, UserUpdate
 from .exceptions import UserWithIdNotFound
-from .crud import create_user, get_user_by_email, get_user_by_id, update_user_by_id, delete_user_by_id, get_all_users
+from .crud import create_user, get_user_by_email, get_user_by_id, update_user_by_id, delete_user_by_id, get_all_users, change_user_is_verify_status
