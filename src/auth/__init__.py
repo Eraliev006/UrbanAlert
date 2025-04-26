33b2d4fdab04e1cc7a8e7c0b5a12ec4a6d12cb33
= <=>
2025-04-26 14:21:19 +0600

    add UserAlreadyVerifiedEmail import

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,5 +1,5 @@
 from .schemas import LoginUserOutput, LoginUserRead, TokenPairs, VerifyEmailSchema
 from .exceptions import (InvalidSignatureException, EmailAlreadyExists,
-                         UserWithEmailNotFound, PasswordIsIncorrect, UserNotVerifyEmail, OTPCodeNotFound, OTPCodeIsWrong)
+                         UserWithEmailNotFound, PasswordIsIncorrect, UserNotVerifyEmail, OTPCodeNotFound, OTPCodeIsWrong, UserAlreadyVerifiedEmail)
 from .utils import hash_password, verify_password
 from .services import register_user, login_user, verify_user_by_otp_code
