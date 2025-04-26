cb01de515757472385e024f948d63ae2c710c331
= <=>
2025-04-26 22:50:19 +0600

    delete LoginUserRead

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,11 +1,6 @@
 from pydantic import EmailStr
 from sqlmodel import SQLModel
 
-
-class LoginUserRead(SQLModel):
-    email: EmailStr
-    password: str
-
 class LoginUserOutput(SQLModel):
     access_token: str
     refresh_token: str
