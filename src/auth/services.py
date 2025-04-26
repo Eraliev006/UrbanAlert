76a53a0af3a5e734f6baef7e3b57d11a37920953
= <=>
2025-04-26 22:51:10 +0600

    refactor login_user service

--- src/auth/services.py
+++ src/auth/services.py
@@ -1,17 +1,18 @@
 from datetime import timedelta
 from typing import Optional
 
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.auth import EmailAlreadyExists, hash_password, LoginUserRead, LoginUserOutput, UserWithEmailNotFound, \
+from src.auth import EmailAlreadyExists, hash_password, LoginUserOutput, \
     verify_password, PasswordIsIncorrect, TokenPairs, UserNotVerifyEmail, VerifyEmailSchema, OTPCodeNotFoundOrExpired, \
-    OTPCodeIsWrong, UserAlreadyVerifiedEmail
+    OTPCodeIsWrong, UserAlreadyVerifiedEmail, UserWithUsernameNotFound, UserWithEmailNotFound
 from src.auth.utils import get_pairs_token, generate_otp_code
 from src.core import redis_client
 from src.notification import NotifierType
 from src.notification.notifier_factory import NotifierFactory
-from src.users import UserCreate, User, UserRead, change_user_is_verify_status
-from src.users import get_user_by_email, create_user
+from src.users import UserCreate, User, UserRead, change_user_is_verify_status, get_user_by_email
+from src.users import get_user_by_username, create_user
 
 
 async def register_user(db_session: AsyncSession, user:UserCreate) -> UserRead:
@@ -55,26 +56,26 @@ async def register_user(db_session: AsyncSession, user:UserCreate) -> UserRead:
     return UserRead(**created_user.model_dump())
 
 
-async def login_user(db_session: AsyncSession,login_data: LoginUserRead) -> LoginUserOutput:
+async def login_user(db_session: AsyncSession,form_data: OAuth2PasswordRequestForm) -> LoginUserOutput:
     """
     Function is login user in app.
-    Return UserWithEmailNotFound exception if email not exists.
+    Return UserWithUsernameNotFound exception if email not exists.
     Return UserNotVerifyEmail if current user is not verify email
     Return PasswordIsIncorrect if password is incorrect
-    Return LoginUserOutput if password and email is correct
-    :param login_data: getting the LoginUserRead instance to check is user registered and check password and email
+    Return LoginUserOutput if password and username is correct
+    :param form_data: getting the OAuth2PasswordRequestForm instance to check is user registered and check password and email
     :param db_session: takes session to make transaction with database
     """
-    exists_user: Optional[User] = await get_user_by_email(db_session, str(login_data.email))
+    exists_user: Optional[User] = await get_user_by_username(db_session, form_data.username)
 
     if not exists_user:
-        raise UserWithEmailNotFound(str(login_data.email))
+        raise UserWithUsernameNotFound(form_data.username)
 
     if not exists_user.is_verified:
         raise UserNotVerifyEmail
 
     if not verify_password(
-        plain_pass=login_data.password.encode(),
+        plain_pass=form_data.password.encode(),
         hashed_password=exists_user.password.encode()
     ):
         raise PasswordIsIncorrect
