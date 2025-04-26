4457a3bae228eceaebae643380d0d2082f9d6378
= <=>
2025-04-26 22:48:59 +0600

    refactor and add new imports

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,5 +1,5 @@
-from .schemas import LoginUserOutput, LoginUserRead, TokenPairs, VerifyEmailSchema
-from .exceptions import (InvalidSignatureException, EmailAlreadyExists,
-                         UserWithEmailNotFound, PasswordIsIncorrect, UserNotVerifyEmail, OTPCodeNotFoundOrExpired, OTPCodeIsWrong, UserAlreadyVerifiedEmail)
+from .schemas import LoginUserOutput, TokenPairs, VerifyEmailSchema
+from .exceptions import (InvalidSignatureException, EmailAlreadyExists, UserWithEmailNotFound,
+                         UserWithUsernameNotFound, PasswordIsIncorrect, UserNotVerifyEmail, OTPCodeNotFoundOrExpired, OTPCodeIsWrong, UserAlreadyVerifiedEmail)
 from .utils import hash_password, verify_password
 from .services import register_user, login_user, verify_user_by_otp_code
