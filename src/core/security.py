e0e400b2e9d9c1bf8bb78fe146be714eb62b886e
= <=>
2025-04-26 22:50:41 +0600

    change HTTPBearer to OAuth2PasswordBearer

--- src/core/security.py
+++ src/core/security.py
@@ -1,19 +1,19 @@
 from typing import Annotated
 
 from fastapi.params import Depends
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.security import OAuth2PasswordBearer
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.auth.utils import decode_token
 from src.core import database_helper
 from src.users import UserRead, get_user_by_id
 
-security = HTTPBearer()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login-user')
 
 async def get_current_user(
-        credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],
+        token: Annotated[str, Depends(oauth2_scheme)],
         db_session: AsyncSession = Depends(database_helper.session_getter),
 ) -> UserRead:
-    decoded_token = decode_token(credentials.credentials)
+    decoded_token = decode_token(token)
     user: UserRead = await get_user_by_id(db_session, decoded_token['user_id'])
     return user
