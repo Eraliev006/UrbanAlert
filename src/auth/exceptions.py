7a75fc57a581dc914f75761339e735b54a8e360a
= <=>
2025-04-26 14:15:55 +0600

    feat[auth.exceptions]: add OTPCodeNotFound exceptions

--- src/auth/exceptions.py
+++ src/auth/exceptions.py
@@ -49,3 +49,10 @@ class UserNotVerifyEmail(HTTPException):
             status_code=status.HTTP_403_FORBIDDEN,
             detail=f'User is not verify email'
         )
+
+class OTPCodeNotFound(HTTPException):
+    def __init__(self, email: str):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'OTP code is not found for email - {email}'
+        )
