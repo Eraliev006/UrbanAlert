75be7f4405e07678edb9d021e996eded4c461299
= <=>
2025-04-27 10:01:09 +0600

    refactor security

--- src/core/security.py
+++ src/core/security.py
@@ -1,19 +1,19 @@
 from typing import Annotated
 
-from fastapi.params import Depends
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
-from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.auth.utils import decode_token
-from src.core import database_helper
-from src.users import UserRead, get_user_by_id
+from src.core import get_user_service
+from src.users import UserRead, UserService
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login-user')
 
 async def get_current_user(
         token: Annotated[str, Depends(oauth2_scheme)],
-        db_session: AsyncSession = Depends(database_helper.session_getter),
+        user_service: Annotated[UserService, Depends(get_user_service)]
+
 ) -> UserRead:
     decoded_token = decode_token(token)
-    user: UserRead = await get_user_by_id(db_session, decoded_token['user_id'])
+    user: UserRead = await user_service.get_user_by_id(decoded_token['user_id'])
     return user
