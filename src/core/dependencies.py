07aa24bea1ab2a3c825edffb1a95035d3f80fcca
= <=>
2025-04-27 23:26:53 +0600

    add get_token_service dependency

--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -6,7 +6,7 @@ from .base_services import Services
 
 from src.users import UserService
 from ..auth import AuthService
-
+from ..tokens import TokenService
 
 
 def get_service(db: AsyncSession = Depends(database_helper.session_getter)) -> Services:
@@ -17,3 +17,6 @@ def get_user_service(service: Services = Depends(get_service)) -> UserService:
 
 def get_auth_service(service: Services = Depends(get_service)) -> AuthService:
     return service.auth_service
+
+def get_token_service(service: Services = Depends(get_service)) -> TokenService:
+    return service.token_service
