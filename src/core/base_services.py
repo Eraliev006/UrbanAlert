6d3a1fce288f0dda4356fe1a241cc5e380cf93e8
= <=>
2025-04-27 22:43:54 +0600

    refactor[core.base_services]: refactor instead importing token_repository import TokenService

--- src/core/base_services.py
+++ src/core/base_services.py
@@ -1,14 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth import AuthService
-from src.auth.tokens.token_repository import TokenRepository
+from src.tokens.token_service import TokenService
 from src.core import redis_client
 from src.users import UserService
 
 
 class Services:
     def __init__(self, db: AsyncSession):
-        self.token_repo = TokenRepository(redis_client)
+        self.token_repo = TokenService(redis_client)
 
         self.user = UserService(db)
         self.auth_service = AuthService(db, self.user,self.token_repo)
