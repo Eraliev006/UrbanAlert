3b4732d51403e4a51c875efca0106ca7a080f5a2
= <=>
2025-04-27 10:19:35 +0600

    refactor[core.security]: fix problem with import

--- src/core/security.py
+++ src/core/security.py
@@ -4,7 +4,7 @@ from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
 from src.auth.utils import decode_token
-from src.core import get_user_service
+from .dependencies import get_user_service
 from src.users import UserRead, UserService
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login-user')
