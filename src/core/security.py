d7e88b8e302a51016edf41e54da8d5dcefd47e30
= <=>
2025-04-28 00:19:56 +0600

    add token type checking

--- src/core/security.py
+++ src/core/security.py
@@ -5,6 +5,7 @@ from fastapi.security import OAuth2PasswordBearer
 
 from .dependencies import get_user_service, get_token_service
 from src.users import UserRead, UserService
+from ..auth import InvalidTokenType
 from ..tokens import TokenService
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login-user')
@@ -15,5 +16,7 @@ async def get_current_user(
         token_service: Annotated[TokenService, Depends(get_token_service)]
 ) -> UserRead:
     decoded_token = token_service.decode_token(token)
+    if decoded_token['type'] != 'access':
+        raise InvalidTokenType
     user: UserRead = await user_service.get_user_by_id(int(decoded_token['sub']))
     return user
