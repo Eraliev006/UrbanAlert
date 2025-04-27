c1f3a0fffb16551c736c5d5dd21a68ec1183009a
= <=>
2025-04-27 11:54:42 +0600

    refactor all exceptions and add new UserWithEmailOrUsernameNotFound

--- src/auth/exceptions.py
+++ src/auth/exceptions.py
@@ -1,86 +1,79 @@
 from fastapi import HTTPException
 from starlette import status
+from typing import Optional
 
 
-class AuthException(HTTPException):
+class BaseHTTPException(HTTPException):
+    def __init__(self, status_code: int, detail: str):
+        super().__init__(status_code=status_code, detail=detail)
+
+
+class AuthException(BaseHTTPException):
     def __init__(self, detail: str):
-        super().__init__(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=detail
-        )
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
 
 
 class InvalidSignatureException(AuthException):
     def __init__(self):
-        super().__init__(
-            detail='Invalid token signature error',
-        )
+        super().__init__(detail='Invalid token signature error')
+
 
 class ExpiredTokenSignatureException(AuthException):
     def __init__(self):
-        super().__init__(
-            detail='Token expired',
-        )
+        super().__init__(detail='Token expired')
 
-class EmailAlreadyExists(HTTPException):
+
+class EmailAlreadyExists(BaseHTTPException):
     def __init__(self, email: str):
-        super().__init__(
-            status_code=status.HTTP_409_CONFLICT,
-            detail=f'Email - {email} already exists'
-        )
+        super().__init__(status_code=status.HTTP_409_CONFLICT, detail=f'Email - {email} already exists')
 
-class UserWithEmailNotFound(HTTPException):
+
+class EmailOrUsernameAlreadyExists(BaseHTTPException):
+    def __init__(self, email: Optional[str], username: Optional[str]):
+        detail = f'Email - {email} or username - {username} already exists'
+        super().__init__(status_code=status.HTTP_409_CONFLICT, detail=detail)
+
+
+class UserWithEmailNotFound(BaseHTTPException):
     def __init__(self, email: str):
-        super().__init__(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'Email - {email} is not found'
-        )
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=f'Email - {email} is not found')
 
-class PasswordIsIncorrect(HTTPException):
+
+class PasswordIsIncorrect(AuthException):
     def __init__(self):
-        super().__init__(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f'Password is incorrect'
-        )
+        super().__init__(detail='Password is incorrect')
 
-class UserNotVerifyEmail(HTTPException):
+
+class UserNotVerifyEmail(BaseHTTPException):
     def __init__(self):
-        super().__init__(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail=f'User is not verify email'
-        )
+        super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail='User is not verified email')
 
-class UserWithUsernameNotFound(HTTPException):
+
+class UserWithUsernameNotFound(BaseHTTPException):
     def __init__(self, username: str):
-        super().__init__(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'Username - {username} is not found'
-        )
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=f'Username - {username} is not found')
 
-class OTPCodeNotFoundOrExpired(HTTPException):
+
+class OTPCodeNotFoundOrExpired(BaseHTTPException):
     def __init__(self, email: str):
-        super().__init__(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'OTP code is not found or expired for email - {email}'
-        )
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=f'OTP code is not found or expired for email - {email}')
 
-class OTPCodeIsWrong(HTTPException):
+
+class OTPCodeIsWrong(BaseHTTPException):
     def __init__(self):
-        super().__init__(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail='OTP code is wrong'
-        )
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail='OTP code is wrong')
 
-class UserAlreadyVerifiedEmail(HTTPException):
+
+class UserAlreadyVerifiedEmail(BaseHTTPException):
     def __init__(self, email: str):
-        super().__init__(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f'User with email - {email} already verify'
-        )
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=f'User with email - {email} already verified')
 
-class InvalidTokenType(HTTPException):
+
+class InvalidTokenType(AuthException):
     def __init__(self):
-        super().__init__(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Token is invalid type'
-        )
+        super().__init__(detail='Token is of invalid type')
+
+
+class RefreshTokenNotFound(AuthException):
+    def __init__(self):
+        super().__init__(detail='Refresh token not found')
