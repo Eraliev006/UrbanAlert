77b931800a6483a112877a6a6adf8b2a538c341c
= <=>
2025-04-27 11:56:34 +0600

    RefreshTokenNotFound import new exception

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,5 +1,5 @@
 from .schemas import LoginUserOutput, TokenPairs, VerifyEmailSchema, RefreshTokenRequest, NewAccessToken
-from .exceptions import (InvalidSignatureException, EmailAlreadyExists, UserWithEmailNotFound, InvalidTokenType,
+from .exceptions import (InvalidSignatureException, EmailAlreadyExists, UserWithEmailNotFound, InvalidTokenType, RefreshTokenNotFound,
                          UserWithUsernameNotFound, PasswordIsIncorrect, UserNotVerifyEmail, OTPCodeNotFoundOrExpired, OTPCodeIsWrong, UserAlreadyVerifiedEmail)
 from .utils import hash_password, verify_password, get_pairs_token, generate_otp_code, decode_token
 from .services import AuthService
