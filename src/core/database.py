8a651f0b2a89c033af2f5b9cdccc7964b622c87a
= <=>
2025-04-27 10:47:46 +0600

    Add AuthService and refactor Services class to initialize both user and auth services

--- src/core/database.py
+++ src/core/database.py
@@ -1,6 +1,10 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.auth import AuthService
 from src.users import UserService
 
 
 class Services:
-    def __init__(self):
-        self.user = UserService()
+    def __init__(self, db: AsyncSession):
+        self.user = UserService(db)
+        self.auth_service = AuthService(db, self.user)
