7ed24a806b6ff0b182b5b12a1e930c2ed24d723a
= <=>
2025-04-27 14:14:49 +0600

    add token_repository to base_service

--- src/core/base_services.py
+++ src/core/base_services.py
@@ -1,10 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth import AuthService
+from src.auth.tokens.token_repository import TokenRepository
+from src.core import redis_client
 from src.users import UserService
 
 
 class Services:
     def __init__(self, db: AsyncSession):
+        self.token_repo = TokenRepository(redis_client)
+
         self.user = UserService(db)
-        self.auth_service = AuthService(db, self.user)
+        self.auth_service = AuthService(db, self.user,self.token_repo)
