5cabdc2ab1073e8ae266e8fa11d088f45f87a50a
= <=>
2025-04-27 22:40:33 +0600

    refactor[auth.schemas]: delete token schemas

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -6,16 +6,6 @@ class LoginUserOutput(SQLModel):
     refresh_token: str
     token_type: str
 
-class TokenPairs(SQLModel):
-    access_token: str
-    refresh_token: str
-
 class VerifyEmailSchema(SQLModel):
     email_user: EmailStr
     otp_code: str
-
-class RefreshTokenRequest(SQLModel):
-    refresh_token: str
-
-class NewAccessToken(SQLModel):
-    new_access_token: str
