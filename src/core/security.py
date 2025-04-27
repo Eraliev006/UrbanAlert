1aa26dc68647b1137613a348c592bdf01020cfcb
= <=>
2025-04-27 23:46:25 +0600

    refactor get_current_user add token_service dependency

--- src/core/security.py
+++ src/core/security.py
@@ -3,17 +3,17 @@ from typing import Annotated
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
-from src.auth.utils import decode_token
-from .dependencies import get_user_service
+from .dependencies import get_user_service, get_token_service
 from src.users import UserRead, UserService
+from ..tokens import TokenService
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login-user')
 
 async def get_current_user(
         token: Annotated[str, Depends(oauth2_scheme)],
-        user_service: Annotated[UserService, Depends(get_user_service)]
-
+        user_service: Annotated[UserService, Depends(get_user_service)],
+        token_service: Annotated[TokenService, Depends(get_token_service)]
 ) -> UserRead:
-    decoded_token = decode_token(token)
-    user: UserRead = await user_service.get_user_by_id(decoded_token['user_id'])
+    decoded_token = token_service.decode_token(token)
+    user: UserRead = await user_service.get_user_by_id(int(decoded_token['sub']))
     return user
