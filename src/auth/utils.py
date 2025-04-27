b3d682c1a2a6abffcb124be9b1b46fddfece4641
= <=>
2025-04-27 22:38:43 +0600

    delete functions working with token

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,43 +1,6 @@
 import random
-from typing import Any
 
-import jwt
-from jwt import InvalidSignatureError, ExpiredSignatureError
 from passlib.context import CryptContext
-
-from src.auth import TokenPairs
-from src.auth.exceptions import ExpiredTokenSignatureException, InvalidSignatureException
-from src.auth.tokens.access import AccessTokenCreator
-from src.auth.tokens.refresh import RefreshTokenCreator
-from src.core import settings
-
-
-def get_pairs_token(payload: dict[str, Any]) -> TokenPairs:
-    access_token = AccessTokenCreator().generate(payload)
-    refresh_token = RefreshTokenCreator().generate(payload)
-
-    return TokenPairs(
-        access_token = access_token,
-        refresh_token = refresh_token
-    )
-
-def decode_token(
-        token: str,
-        algorithm: str = settings.jwt.algorithm,
-        secret_key = settings.jwt.secret_key
-):
-    try:
-        return jwt.decode(
-            jwt = token,
-            algorithms=[algorithm],
-            key=secret_key,
-        )
-
-    except InvalidSignatureError:
-        raise InvalidSignatureException
-
-    except ExpiredSignatureError:
-        raise ExpiredTokenSignatureException
 
 def hash_password(password: bytes) -> str:
     """Hash password"""
