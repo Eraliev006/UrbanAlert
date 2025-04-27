c32d630fccc35768689801b4fcbf485c90571879
= <=>
2025-04-27 22:44:29 +0600

    add import and deleting unuseful import

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,5 +1,5 @@
-from .schemas import LoginUserOutput, TokenPairs, VerifyEmailSchema, RefreshTokenRequest, NewAccessToken
+from .schemas import LoginUserOutput, VerifyEmailSchema
 from .exceptions import (InvalidSignatureException, EmailAlreadyExists, UserWithEmailNotFound, InvalidTokenType, RefreshTokenNotFound, EmailOrUsernameAlreadyExists,
                          UserWithUsernameNotFound, PasswordIsIncorrect, UserNotVerifyEmail, OTPCodeNotFoundOrExpired, OTPCodeIsWrong, UserAlreadyVerifiedEmail)
-from .utils import hash_password, verify_password, get_pairs_token, generate_otp_code, decode_token
+from .utils import hash_password, verify_password, generate_otp_code
 from .services import AuthService
