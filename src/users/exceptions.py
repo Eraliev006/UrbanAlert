aa9f1749572fb01b0a210781dca20a4f89246a99
= <=>
2025-04-29 18:46:39 +0600

    refactor(users): move user-related exceptions from auth to users module

--- src/users/exceptions.py
+++ src/users/exceptions.py
@@ -1,5 +1,9 @@
+from typing import Optional
+
 from fastapi import HTTPException
 from starlette import status
+
+from src.common import BaseHTTPException
 
 
 class UserWithIdNotFound(HTTPException):
@@ -8,3 +12,36 @@ class UserWithIdNotFound(HTTPException):
             detail=f'User with {user_id}-ID not found',
             status_code=status.HTTP_404_NOT_FOUND
         )
+
+class EmailAlreadyExists(BaseHTTPException):
+    def __init__(self, email: str):
+        super().__init__(status_code=status.HTTP_409_CONFLICT, detail=f'Email - {email} already exists')
+
+
+class EmailOrUsernameAlreadyExists(BaseHTTPException):
+    def __init__(self, email: Optional[str], username: Optional[str]):
+        detail = f'Email - {email} or username - {username} already exists'
+        super().__init__(status_code=status.HTTP_409_CONFLICT, detail=detail)
+
+
+class UserWithEmailNotFound(BaseHTTPException):
+    def __init__(self, email: str):
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=f'Email - {email} is not found')
+
+class UserNotVerifyEmail(BaseHTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail='User is not verified email')
+
+
+class UserWithUsernameNotFound(BaseHTTPException):
+    def __init__(self, username: str):
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=f'Username - {username} is not found')
+
+
+class UserAlreadyVerifiedEmail(BaseHTTPException):
+    def __init__(self, email: str):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=f'User with email - {email} already verified')
+
+class UserWithUsernameAlreadyExists(BaseHTTPException):
+    def __init__(self, username: str):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=f'User with username - {username} already exists')
