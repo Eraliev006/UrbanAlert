97f4f79504c34d128baa4f3e35549a894699c04b
= <=>
2025-04-29 18:44:35 +0600

    add imports exceptions

--- src/users/__init__.py
+++ src/users/__init__.py
@@ -1,3 +1,3 @@
 from .models import User, UserCreate, UserRead, UserUpdate
-from .exceptions import UserWithIdNotFound
+from .exceptions import UserWithIdNotFound, UserWithEmailNotFound, UserAlreadyVerifiedEmail, UserNotVerifyEmail, UserWithUsernameNotFound, EmailOrUsernameAlreadyExists, UserWithUsernameAlreadyExists, EmailAlreadyExists
 from .user_services import UserService
