6fe6f5193de72725c5d717ea0a3a65fc1eb163ba
= <=>
2025-05-01 13:47:23 +0600

    rename complaint_repositories to complaint_service import complaint_service

--- src/complaints/__init__.py
+++ src/complaints/__init__.py
@@ -1,3 +1,4 @@
 from .models import Complaint
 from .complaint_status import ComplaintStatus
 from .exceptions import ComplaintWithIdNotFound, AccessDenied
+from .complaint_services import ComplaintService
