821206429f3c84f46638ef8222a4802178f6df5f
= <=>
2025-05-01 13:20:57 +0600

    feat(common): add base not found excetpions

--- src/common/exceptions.py
+++ src/common/exceptions.py
@@ -20,5 +20,10 @@ class BaseHTTPException(HTTPException):
         super().__init__(status_code=status_code, detail=detail)
 
 
+class NotFoundException(BaseHTTPException):
+    def __init__(self, detail: str):
+        super().__init__(status_code = status.HTTP_404_NOT_FOUND, detail=detail)
+
+
 class ErrorResponse(BaseModel):
     detail: str
