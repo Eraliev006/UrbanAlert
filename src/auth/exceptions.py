b19f1c12ea3f7a7350f875eaae851c9c7650846f
= <=>
2025-05-01 18:32:52 +0600

    move AuthException to common.exceptions

--- src/auth/exceptions.py
+++ src/auth/exceptions.py
@@ -1,21 +1,6 @@
 from starlette import status
 
-from src.common import BaseHTTPException
-
-
-class AuthException(BaseHTTPException):
-    def __init__(self, detail: str):
-        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
-
-
-class InvalidSignatureException(AuthException):
-    def __init__(self):
-        super().__init__(detail='Invalid token signature error')
-
-
-class ExpiredTokenSignatureException(AuthException):
-    def __init__(self):
-        super().__init__(detail='Token expired')
+from src.common import BaseHTTPException, AuthException
 
 
 class PasswordIsIncorrect(AuthException):
@@ -33,11 +18,3 @@ class OTPCodeIsWrong(BaseHTTPException):
         super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail='OTP code is wrong')
 
 
-class InvalidTokenType(AuthException):
-    def __init__(self):
-        super().__init__(detail='Token is of invalid type')
-
-
-class RefreshTokenNotFound(AuthException):
-    def __init__(self):
-        super().__init__(detail='Refresh token not found')
--- src/common/exceptions.py
+++ src/common/exceptions.py
@@ -20,6 +20,10 @@ class BaseHTTPException(HTTPException):
         super().__init__(status_code=status_code, detail=detail)
 
 
+class AuthException(BaseHTTPException):
+    def __init__(self, detail: str):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
+
 class NotFoundException(BaseHTTPException):
     def __init__(self, detail: str):
         super().__init__(status_code = status.HTTP_404_NOT_FOUND, detail=detail)
