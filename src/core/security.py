2ece188e54c3ec5858cce6a35f0b27d6e2682112
= <=>
2025-05-01 18:34:18 +0600

    feat(core.security): change function from decode_token to decode_token_with_token_type_checking.

--- src/core/security.py
+++ src/core/security.py
@@ -5,8 +5,7 @@ from fastapi.security import OAuth2PasswordBearer
 
 from .dependencies import get_user_service, get_token_service
 from src.users import UserRead, UserService
-from ..auth import InvalidTokenType
-from ..tokens import TokenService
+from ..tokens import TokenService, TokenType
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login-user')
 
@@ -15,8 +14,6 @@ async def get_current_user(
         user_service: Annotated[UserService, Depends(get_user_service)],
         token_service: Annotated[TokenService, Depends(get_token_service)]
 ) -> UserRead:
-    decoded_token = token_service.decode_token(token)
-    if decoded_token['type'] != 'access':
-        raise InvalidTokenType
+    decoded_token = token_service.decode_token_with_token_type_checking(token, TokenType.access)
     user: UserRead = await user_service.get_user_by_id(int(decoded_token['sub']))
     return user
