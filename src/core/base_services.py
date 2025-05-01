dd54bf49512a1696cf17993806838417ccec187e
= <=>
2025-05-01 16:14:37 +0600

    Add complaint service to Services

--- src/core/base_services.py
+++ src/core/base_services.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth import AuthService
+from src.complaints import ComplaintService
 from src.tokens.token_service import TokenService
 from src.core import redis_client
 from src.users import UserService
@@ -10,5 +11,6 @@ class Services:
     def __init__(self, db: AsyncSession):
         self.token_service = TokenService(redis_client)
 
+        self.complaint_service = ComplaintService(db)
         self.user = UserService(db)
         self.auth_service = AuthService(db, self.user,self.token_service)
