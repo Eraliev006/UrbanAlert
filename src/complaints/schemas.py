0b74a72fec41a7e3eedc29dad4db42eda1846064
= <=>
2025-05-02 16:12:44 +0600

    fear(complaints): move ComplaintBase Complaint Create ComplaintRead ComplaintUpdate to schemas from models. And add complaintQueryModel

---
+++ src/complaints/schemas.py
@@ -0,0 +1,35 @@
+import datetime
+from typing import Optional
+
+from sqlmodel import SQLModel, Field
+from .complaint_status import ComplaintStatus
+
+
+class ComplaintBase(SQLModel):
+    complaint_text: str
+    category: Optional[str] = None
+    image_url: str = Field(nullable=True)
+    latitude: float = Field(nullable=False)
+    longitude: float = Field(nullable=False)
+    description:str = Field(nullable=True)
+
+class ComplaintCreate(ComplaintBase):
+    status: Optional[ComplaintStatus] = Field(default=ComplaintStatus.PENDING)
+
+
+class ComplaintRead(ComplaintBase):
+    id: int
+    user_id: int
+    status: ComplaintStatus
+    created_at: datetime.date
+    updated_at: datetime.date
+
+
+class ComplaintUpdate(SQLModel):
+    status: Optional[ComplaintStatus] = ComplaintStatus.PENDING
+
+
+class ComplaintQueryModel(SQLModel):
+    category: Optional[str] = None
+    status: Optional[ComplaintStatus] = ComplaintStatus.PENDING
+
