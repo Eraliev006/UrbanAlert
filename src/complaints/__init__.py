e717633d84234b53a25d4214fc9f2c4adb881144
= <=>
2025-05-02 16:11:05 +0600

    refactor imports

--- src/complaints/__init__.py
+++ src/complaints/__init__.py
@@ -1,4 +1,5 @@
 from src.complaints.complaint_status import ComplaintStatus
-from src.complaints.models import Complaint, ComplaintRead, ComplaintUpdate, ComplaintCreate
+from src.complaints.models import Complaint
+from .schemas import ComplaintUpdate, ComplaintCreate, ComplaintRead,ComplaintBase, ComplaintQueryModel
 from src.complaints.exceptions import ComplaintWithIdNotFound, AccessDenied
 from src.complaints.complaint_services import ComplaintService
