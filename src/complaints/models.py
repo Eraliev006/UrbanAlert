2ca9ff4f8ac10740f151bca1f1a5fad59d3fddab
= <=>
2025-05-02 16:12:19 +0600

    fear(complaints): move ComplaintBase Complaint Create ComplaintRead ComplaintUpdate to schemas from models

--- src/complaints/models.py
+++ src/complaints/models.py
@@ -1,20 +1,13 @@
 import datetime
 from typing import Optional, TYPE_CHECKING
 
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import Field, Relationship
+
+from .schemas import ComplaintBase
 from .complaint_status import ComplaintStatus
 
 if TYPE_CHECKING:
     from src import User
-
-
-class ComplaintBase(SQLModel):
-    complaint_text: str
-    category: Optional[str] = None
-    image_url: str = Field(nullable=True)
-    latitude: float = Field(nullable=False)
-    longitude: float = Field(nullable=False)
-    description:str = Field(nullable=True)
 
 
 class Complaint(ComplaintBase, table=True):
@@ -30,17 +23,3 @@ class Complaint(ComplaintBase, table=True):
     user: "User" = Relationship(back_populates='complaints')
 
 
-class ComplaintCreate(ComplaintBase):
-    status: Optional[ComplaintStatus] = Field(default=ComplaintStatus.PENDING)
-
-
-class ComplaintRead(ComplaintBase):
-    id: int
-    user_id: int
-    status: ComplaintStatus
-    created_at: datetime.date
-    updated_at: datetime.date
-
-
-class ComplaintUpdate(SQLModel):
-    status: Optional[ComplaintStatus] = ComplaintStatus.PENDING
