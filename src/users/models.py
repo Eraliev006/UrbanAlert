17f42f51ee34d970214e1535a3393967851206cd
= <=>
2025-05-03 16:10:31 +0600

    refactor(users.models): move schemas to schemas file

--- src/users/models.py
+++ src/users/models.py
@@ -1,17 +1,14 @@
 import datetime
 from typing import Optional, TYPE_CHECKING
 
-from pydantic import EmailStr
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import Field, Relationship
+
+from src.users import BaseUser
 
 if TYPE_CHECKING:
     from src import Complaint
 
 
-class BaseUser(SQLModel):
-    username: str = Field(max_length=100, unique=True)
-    email: EmailStr = Field(unique=True, index=True, nullable=False)
-    avatar_url: Optional[str] = Field(nullable=True)
 
 class User(BaseUser, table = True):
     __tablename__ = "user"
@@ -24,19 +21,7 @@ class User(BaseUser, table = True):
 
     complaints: list["Complaint"] = Relationship(back_populates='user')
 
-class UserCreate(BaseUser):
-    password: str
 
-class UserRead(BaseUser):
-    id: int
-    is_verified: bool
-    created_at: datetime.date
-
-class UserUpdate(SQLModel):
-    username: str
-    email: Optional[EmailStr]
-    avatar_url: Optional[str]
-    is_verified: Optional[bool] = False
 
 
 
