f5b6c0b46ef3bc5af020e56319f14b54975ffbe7
= <=>
2025-05-03 16:33:06 +0600

    refactor(auth): use user repository and services for cleaner responsibilities

--- src/auth/services.py
+++ src/auth/services.py
@@ -2,26 +2,37 @@ from datetime import timedelta
 from typing import Optional
 
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from src import User
-from src.auth import hash_password, LoginUserOutput, \
-    verify_password, PasswordIsIncorrect,VerifyEmailSchema, OTPCodeNotFoundOrExpired, \
-    OTPCodeIsWrong, generate_otp_code
+
+from .utils import hash_password, verify_password, generate_otp_code
+from .schemas import LoginUserOutput, VerifyEmailSchema
+from .exceptions import PasswordIsIncorrect, OTPCodeNotFoundOrExpired, OTPCodeIsWrong
+
 from src.notification import EmailNotificationService
 from src.tokens import TokenService, TokenType, RefreshTokenNotFound, InvalidSignatureException
 from src.core import redis_client
-from src.users import UserCreate, UserRead, UserService, UserWithUsernameNotFound, UserNotVerifyEmail, \
-    UserWithEmailNotFound, UserAlreadyVerifiedEmail
-from src.users import EmailOrUsernameAlreadyExists
+
+from src.users.schemas import UserCreate, UserRead
+from src.users.user_services import UserService
+from src.users.repositories import UserRepositories
+from src.users.exceptions import UserWithUsernameNotFound, UserNotVerifyEmail, UserWithEmailNotFound, \
+    UserAlreadyVerifiedEmail
 
 
 class AuthService:
-    def __init__(self,db: AsyncSession, user_service: UserService, token_service: TokenService, email_service: EmailNotificationService):
-        self.token_service = token_service
-        self.user_service = user_service
-        self.email_service = email_service
-        self.db = db
+    def __init__(
+
+            self,
+            user_service: UserService,
+            token_service: TokenService,
+            email_service: EmailNotificationService,
+            user_repo: UserRepositories,
+    ):
+        self._token_service = token_service
+        self._user_service = user_service
+        self._email_service = email_service
+        self._user_repo = user_repo
 
     async def register_user(self, user: UserCreate) -> UserRead:
         """
@@ -29,35 +40,28 @@ class AuthService:
         Return EmailAlreadyExists exception if email already exists. Also return UserRead
         :param user: getting the UserCreate instance to insert to db
         """
-        exists_user: Optional[User] = await self.user_service.get_user_by_email_or_username(str(user.email), user.username)
 
-        if exists_user:
-            raise EmailOrUsernameAlreadyExists(str(user.email), user.username)
-
-        hashed_password: str = hash_password(user.password.encode())
         user = UserCreate(
             **user.model_dump(exclude={'password'}),
-            password=hashed_password
+            password=hash_password(user.password.encode())
         )
 
-        created_user: Optional[User] = await self.user_service.create_user(
-            user
-        )
+        created = await self._user_service.create_user(user)
 
         otp_code = generate_otp_code()
 
-        await self.email_service.send_otp(
-            to_email=str(created_user.email),
+        await self._email_service.send_otp(
+            to_email=str(created.email),
             otp_code=otp_code
         )
 
         await redis_client.set(
-            key=f'otp:{created_user.email}',
+            key=f'otp:{created.email}',
             value=otp_code,
             ex=timedelta(minutes=5)
         )
 
-        return UserRead(**created_user.model_dump())
+        return created
 
     async def login_user(self, form_data: OAuth2PasswordRequestForm) -> LoginUserOutput:
         """
@@ -66,7 +70,7 @@ class AuthService:
         :param form_data: OAuth2PasswordRequestForm instance with username and password.
         :return: LoginUserOutput containing access and refresh tokens.
         """
-        exists_user: Optional[User] = await self.user_service.get_user_by_username(form_data.username)
+        exists_user: Optional[User] = await self._user_repo.get_by_username(form_data.username)
 
         if not exists_user:
             raise UserWithUsernameNotFound(form_data.username)
@@ -80,9 +84,9 @@ class AuthService:
         ):
             raise PasswordIsIncorrect
 
-        access_token, refresh_token = self.token_service.get_access_and_refresh_tokens(exists_user)
+        access_token, refresh_token = self._token_service.get_access_and_refresh_tokens(exists_user)
 
-        await self.token_service.save_refresh_token(exists_user.id, refresh_token)
+        await self._token_service.save_refresh_token(exists_user.id, refresh_token)
 
         return LoginUserOutput(
             access_token=access_token,
@@ -92,7 +96,7 @@ class AuthService:
 
 
     async def verify_user_by_otp_code(self, verify_data: VerifyEmailSchema) -> dict[str, str]:
-        user = await self.user_service.get_user_by_email(str(verify_data.email_user))
+        user = await self._user_repo.get_by_email(str(verify_data.email_user))
 
         if not user:
             raise UserWithEmailNotFound(str(verify_data.email_user))
@@ -107,14 +111,14 @@ class AuthService:
         if saved_otp_code != verify_data.otp_code:
             raise OTPCodeIsWrong
 
-        await self.user_service.change_user_is_verify_status(user)
+        await self._user_repo.set_verified(user, verified=True)
         return {"message": "User verification successful"}
 
     async def refresh_token(self, refresh_token: str) -> LoginUserOutput:
-        payload = self.token_service.decode_token_with_token_type_checking(refresh_token, TokenType.refresh)
+        payload = self._token_service.decode_token_with_token_type_checking(refresh_token, TokenType.refresh)
         user_id = int(payload['sub'])
 
-        exists_refresh_token = await self.token_service.get_refresh_token(user_id)
+        exists_refresh_token = await self._token_service.get_refresh_token(user_id)
 
         if not exists_refresh_token:
             raise RefreshTokenNotFound
@@ -122,12 +126,12 @@ class AuthService:
         if exists_refresh_token != refresh_token:
             raise InvalidSignatureException
 
-        await self.token_service.delete_refresh_token(user_id)
-        user: UserRead = await self.user_service.get_user_by_id(int(payload['sub']))
+        await self._token_service.delete_refresh_token(user_id)
+        user: UserRead = await self._user_service.get_user_by_id(int(payload['sub']))
 
-        new_access_token, new_refresh_token = self.token_service.get_access_and_refresh_tokens(user)
+        new_access_token, new_refresh_token = self._token_service.get_access_and_refresh_tokens(user)
 
-        await self.token_service.save_refresh_token(user_id, new_refresh_token)
+        await self._token_service.save_refresh_token(user_id, new_refresh_token)
         return LoginUserOutput(
             access_token = new_access_token,
             refresh_token = new_refresh_token,
