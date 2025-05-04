438d4298669390dcd7340436d3f2b09250720184
= <=>
2025-05-05 01:01:49 +0600

    feat(common): add update_instance method to update instance

--- src/common/base_repository.py
+++ src/common/base_repository.py
@@ -1,6 +1,18 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from .db_decorators import db_exception_handler
+
 
 class BaseRepository:
     def __init__(self, db: AsyncSession):
         self.db = db
+
+    @db_exception_handler
+    async def update_instance(self, instance, update_data):
+        for key, value in update_data.model_dump().items():
+            if value is not None:
+                setattr(instance, key, value)
+
+        await self.db.commit()
+        await self.db.refresh(instance)
+        return instance
