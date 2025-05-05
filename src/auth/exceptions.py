6bb0fd08703ebe7f0d078353fde2180a6022a2e9
= <=>
2025-05-05 18:21:46 +0600

    move OTP exceptions from auth.exceptions

--- src/auth/exceptions.py
+++ src/auth/exceptions.py
@@ -1,20 +1,8 @@
-from starlette import status
-
-from src.common import BaseHTTPException, AuthException
+from src.common import AuthException
 
 
 class PasswordIsIncorrect(AuthException):
     def __init__(self):
         super().__init__(detail='Password is incorrect')
-
-
-class OTPCodeNotFoundOrExpired(BaseHTTPException):
-    def __init__(self, email: str):
-        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=f'OTP code is not found or expired for email - {email}')
-
-
-class OTPCodeIsWrong(BaseHTTPException):
-    def __init__(self):
-        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail='OTP code is wrong')
 
 
