e44b1db6b051f0cfbc469833c0802ebe5a58be25
= <=>
2025-05-06 01:56:49 +0600

    feat(core): add dependency of get_comment_service

--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -8,13 +8,14 @@ from src.users import UserService
 from src.auth import AuthService
 from src.complaints import ComplaintService
 from src.tokens import TokenService
+from ..comments.services import CommentService
 
 
 def get_service(db: AsyncSession = Depends(database_helper.session_getter)) -> Services:
     return Services(db)
 
 def get_user_service(service: Services = Depends(get_service)) -> UserService:
-    return service.user
+    return service.user_service
 
 def get_auth_service(service: Services = Depends(get_service)) -> AuthService:
     return service.auth_service
@@ -24,3 +25,6 @@ def get_token_service(service: Services = Depends(get_service)) -> TokenService:
 
 def get_complaint_service(service: Services = Depends(get_service)) -> ComplaintService:
     return service.complaint_service
+
+def get_comment_service(service: Services = Depends(get_service)) -> CommentService:
+    return service.comment_service
