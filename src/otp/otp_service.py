2f5a5719c60a113e12816abd2e33bb84052c7f3c
= <=>
2025-05-05 18:21:59 +0600

    feat(otp): create OTPService

---
+++ src/otp/otp_service.py
@@ -0,0 +1,56 @@
+import random
+
+from fastapi import HTTPException
+from starlette import status
+
+from src.core.redis_client import RedisClient
+from src.notification.notification_strategy import NotificationStrategy
+
+
+class OTPService:
+    def __init__(self, notifier: NotificationStrategy, redis_client: RedisClient):
+        self.notifier = notifier
+        self.redis_client = redis_client
+        self.redis_ttl = 300
+
+    @staticmethod
+    def _generate_otp_code(length: int = 6) -> str:
+        return ''.join([str(random.randint(0, 9)) for _ in range(length)])
+
+    async def send_and_save_otp(self, email: str) -> bool:
+        otp_code = self._generate_otp_code()
+        subject = "Your One-Time Password"
+        body = f"Hello!\n\nYour OTP code is: {otp_code}\n\nRegards."
+
+        success = await self.notifier.notify(email, subject, body)
+        if success:
+            await self.save_otp(email, otp_code)
+
+        return success
+
+    async def save_otp(self, email:str, code:str) -> None:
+        return await self.redis_client.set(
+            key = f"otp:{email}",
+            value=code,
+            ex=self.redis_ttl
+        )
+
+    async def verify_otp(self, email: str, code: str) -> bool:
+        key = f'otp:{email}'
+        stored_code = await self.redis_client.get(key=key)
+
+        if not stored_code:
+            raise HTTPException(status_code=404, detail="OTP not found or expired")
+
+        if stored_code != code:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='OTP is incorrect'
+            )
+
+        await self.redis_client.delete(key=key)
+        return True
+
+
+
+
