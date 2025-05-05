d7c922c3801ec4b69519ffe89a596ebb1ce49686
= <=>
2025-05-06 01:50:01 +0600

    feat(comments): create comment service and create some crud method

---
+++ src/comments/services.py
@@ -0,0 +1,41 @@
+from src.comments.models import Comment
+from src.comments.repositories import CommentRepositories
+from src.comments.schemas import CommentCreate
+from src.complaints import ComplaintService
+from .exceptions import CommentNotFound
+
+
+class CommentService:
+    def __init__(
+        self,
+        comment_repo: CommentRepositories,
+        complaint_service: ComplaintService,
+    ):
+        self._comment_repo = comment_repo
+        self._complaint_service = complaint_service
+
+
+
+    async def create_comment(self, comment_data: CommentCreate, user_id: int) -> Comment:
+        await self._complaint_service.get_by_id(comment_data.complaint_id)
+
+        comment = Comment(
+            user_id=user_id,
+            complaint_id=comment_data.complaint_id,
+            content=comment_data.content
+        )
+        return await self._comment_repo.create(comment)
+
+    async def get_comments_by_complaint(self, complaint_id: int) -> list[Comment]:
+        await self._complaint_service.get_by_id(complaint_id)
+        return await self._comment_repo.get_by_complaint_id(complaint_id)
+
+    async def delete_comment(self, comment_id: int, user_id: int) -> bool:
+        comment = await self._comment_repo.get_by_id(comment_id)
+        if not comment:
+            raise CommentNotFound(comment_id)
+
+        if comment.user_id != user_id:
+            raise PermissionError("You do not have permission to delete this comment.")
+
+        return await self._comment_repo.delete(comment)
