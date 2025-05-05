90d8bdd3a2768c1feb1ffc3bdc87f01f79ca8ae4
= <=>
2025-05-05 18:36:01 +0600

    Refactor service initialization to include repository dependencies
    - Added repositories as dependencies for related services.
    - Improved service initialization by passing repositories explicitly.
    - Enhanced type hinting for better clarity and maintainability.

--- src/core/base_services.py
+++ src/core/base_services.py
@@ -1,18 +1,29 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from src.auth import AuthService
 from src.complaints import ComplaintService
-from src.notification import EmailNotificationService
+from src.complaints.repositories import ComplaintRepositories
+from src.notification import EmailNotification
+from src.otp import OTPService
 from src.tokens.token_service import TokenService
 from src.core import redis_client
-from src.users import UserService
+from src.users import UserService, UserRepositories
 
 
 class Services:
     def __init__(self, db: AsyncSession):
         self.token_service = TokenService(redis_client)
 
-        self.complaint_service = ComplaintService(db)
-        self.user = UserService(db)
-        self.email_service = EmailNotificationService()
-        self.auth_service = AuthService(db, self.user,self.token_service, email_service=self.email_service)
+        self.user_repository = UserRepositories(db)
+        self.user_service = UserService(user_repo=self.user_repository)
+
+        self.complaint_repository = ComplaintRepositories(db)
+        self.complaint_service = ComplaintService(self.complaint_repository, self.user_service)
+
+        self.email_service = EmailNotification()
+        self.otp_service = OTPService(self.email_service, redis_client)
+
+        self.auth_service = AuthService(
+            user_service=self.user_service,
+            token_service=self.token_service,
+            otp_service=self.otp_service
+        )
