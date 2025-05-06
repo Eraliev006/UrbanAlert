f079176b29a0c4913fdeeef49d7657de5ee5262c
= <=>
2025-05-07 00:44:52 +0600

    feat(notification) create notification service

---
+++ src/notification/services.py
@@ -0,0 +1,12 @@
+from src.notification import NotificationStrategy
+
+
+class NotificationService:
+    def __init__(self, strategy: NotificationStrategy):
+        self._strategy = strategy
+
+    def set_strategy(self, strategy: NotificationStrategy):
+        self._strategy = strategy
+
+    async def send_notification(self, recipient: str, subject: str, message: str):
+        return await self._strategy.notify(recipient, subject, message)
