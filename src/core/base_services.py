16ff85a3373476fe977ee138c61a2960c1249eb4
= <=>
2025-05-07 00:40:07 +0600

    Refactor Services initialization to include WebSocket notifications and improve dependency injection

--- src/core/base_services.py
+++ src/core/base_services.py
@@ -4,16 +4,20 @@ from src.comments import CommentRepositories
 from src.comments.services import CommentService
 from src.complaints import ComplaintService
 from src.complaints.repositories import ComplaintRepositories
-from src.notification import EmailNotification
+from src.notification import EmailNotification, NotificationService
+from src.notification.websocket_notification import WebSocketNotification
 from src.otp import OTPService
 from src.tokens.token_service import TokenService
 from src.core import redis_client
 from src.users import UserService, UserRepositories
+from src.websocket import manager
 
 
 class Services:
     def __init__(self, db: AsyncSession):
         self.token_service = TokenService(redis_client)
+        self.notification_service = NotificationService(EmailNotification())
+        self.otp_service = OTPService(self.notification_service, redis_client)
 
         self.user_repository = UserRepositories(db)
         self.user_service = UserService(user_repo=self.user_repository)
@@ -22,10 +26,8 @@ class Services:
         self.complaint_service = ComplaintService(self.complaint_repository, self.user_service)
 
         self.comment_repository = CommentRepositories(db)
-        self.comment_service = CommentService(self.comment_repository, self.complaint_service)
-
-        self.email_service = EmailNotification()
-        self.otp_service = OTPService(self.email_service, redis_client)
+        self.notification_service.set_strategy(WebSocketNotification(connection_manager=manager))
+        self.comment_service = CommentService(self.comment_repository, self.complaint_service, self.notification_service)
 
         self.auth_service = AuthService(
             user_service=self.user_service,
