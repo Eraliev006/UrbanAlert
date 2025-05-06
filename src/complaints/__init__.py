c6868ee2673eb64375f70b2873de5e59b7ee592f
= <=>
2025-05-06 16:41:47 +0600

    import ComplaintReadDetailsSchemas

--- src/complaints/__init__.py
+++ src/complaints/__init__.py
@@ -1,5 +1,5 @@
 from src.complaints.complaint_status import ComplaintStatus
 from src.complaints.models import Complaint
-from .schemas import ComplaintUpdate, ComplaintCreate, ComplaintRead,ComplaintBase, ComplaintQueryModel
+from .schemas import ComplaintUpdate, ComplaintCreate, ComplaintRead,ComplaintBase, ComplaintQueryModel, ComplaintReadDetailsSchemas
 from src.complaints.exceptions import ComplaintWithIdNotFound, AccessDenied
 from src.complaints.complaint_services import ComplaintService
