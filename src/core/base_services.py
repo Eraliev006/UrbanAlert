e1ffa1dc1672cf90c550c3e222bda1494f14bda9
= <=>
2025-05-07 00:52:26 +0600

    Refactor Services: Removed direct notification strategy instantiation, allowing for flexible strategy setting in NotificationService.

--- src/core/base_services.py
+++ src/core/base_services.py
@@ -4,19 +4,17 @@ from src.comments import CommentRepositories
 from src.comments.services import CommentService
 from src.complaints import ComplaintService
 from src.complaints.repositories import ComplaintRepositories
-from src.notification import EmailNotification, NotificationService
-from src.notification.websocket_notification import WebSocketNotification
+from src.notification import NotificationService
 from src.otp import OTPService
 from src.tokens.token_service import TokenService
 from src.core import redis_client
 from src.users import UserService, UserRepositories
-from src.websocket import manager
 
 
 class Services:
     def __init__(self, db: AsyncSession):
         self.token_service = TokenService(redis_client)
-        self.notification_service = NotificationService(EmailNotification())
+        self.notification_service = NotificationService()
         self.otp_service = OTPService(self.notification_service, redis_client)
 
         self.user_repository = UserRepositories(db)
@@ -26,7 +24,6 @@ class Services:
         self.complaint_service = ComplaintService(self.complaint_repository, self.user_service)
 
         self.comment_repository = CommentRepositories(db)
-        self.notification_service.set_strategy(WebSocketNotification(connection_manager=manager))
         self.comment_service = CommentService(self.comment_repository, self.complaint_service, self.notification_service)
 
         self.auth_service = AuthService(
