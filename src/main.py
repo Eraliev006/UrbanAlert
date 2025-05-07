bcaffd63e7b0c679e236fbb36955ba5bb877a0d9
= <=>
2025-05-07 12:43:52 +0600

    include websocket router

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 
 from src.core import settings, redis_client, database_helper
 from auth.routes import router as auth_router
+from src.websocket.routes import router as websocket_router
 from users.routes import router as user_router
 from complaints.complaint_routes import router as complaint_router
 from comments.routes import router as comment_router
@@ -24,6 +25,7 @@ app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(complaint_router)
 app.include_router(comment_router)
+app.include_router(websocket_router)
 
 if __name__ == "__main__":
     uvicorn.run(
