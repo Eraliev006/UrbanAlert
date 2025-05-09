652f71b1ce16f342bde43b6034248a66c8859200
= <=>
2025-05-10 01:42:28 +0600

    feat(comments): add logging

--- src/comments/repositories.py
+++ src/comments/repositories.py
@@ -1,38 +1,47 @@
+import logging
 from sqlmodel import select
 
 from src.common import BaseRepository, db_exception_handler
 from src.comments.models import Comment
 
 
+logger = logging.getLogger('fixkg.comment_repository')
+
 class CommentRepositories(BaseRepository):
 
     @db_exception_handler
     async def create(self, comment: Comment) -> Comment | None:
+        logger.debug('Creating comment: %s', comment)
         self.db.add(comment)
         await self.db.commit()
         await self.db.refresh(comment)
+        logger.info('Comment created with ID: %d', comment.id)
 
         return comment
 
     @db_exception_handler
     async def get_by_complaint_id(self, complaint_id: int) -> list[Comment]:
+        logger.debug('Getting comments for complaint_id: %d', complaint_id)
         stmt = select(Comment).where(Comment.complaint_id == complaint_id)
         comments = await self.db.scalars(stmt)
+        logger.info('Found %d comments for complaint_id: %d', len(comments), complaint_id)
 
         return list(comments)
 
-
     @db_exception_handler
     async def delete(self, comment: Comment) -> bool:
+        logger.debug('Deleting comment with ID: %d', comment.id)
         await self.db.delete(comment)
         await self.db.commit()
+        logger.info('Comment with ID %d deleted', comment.id)
 
         return True
 
-
     @db_exception_handler
     async def get_by_id(self, comment_id: int) -> Comment:
+        logger.debug('Getting comment with ID: %d', comment_id)
         stmt = select(Comment).where(Comment.id == comment_id)
         comment = await self.db.scalar(stmt)
+        logger.info('Comment retrieved with ID: %d', comment.id if comment else 'Not Found')
 
         return comment
--- src/comments/services.py
+++ src/comments/services.py
@@ -1,3 +1,4 @@
+import logging
 from src.comments.models import Comment
 from src.comments.repositories import CommentRepositories
 from src.comments.schemas import CommentCreate
@@ -7,6 +8,7 @@ from src.notification import NotificationService
 from src.notification.websocket_notification import WebSocketNotification
 from src.websocket import manager
 
+logger = logging.getLogger('fixkg.comment_service')
 
 class CommentService:
     def __init__(
@@ -19,32 +21,46 @@ class CommentService:
         self._complaint_service = complaint_service
         self._notification_service = notification_service
 
-
     async def create_comment(self, comment_data: CommentCreate, user_id: int) -> Comment:
+        logger.debug('Creating comment for user_id=%d, complaint_id=%d', user_id, comment_data.complaint_id)
         complaint = await self._complaint_service.get_by_id(comment_data.complaint_id)
 
         comment = Comment(
             user_id=user_id,
             **comment_data.model_dump()
         )
+
         self._notification_service.set_strategy(WebSocketNotification(connection_manager=manager))
         await self._notification_service.send_notification(
             recipient=str(complaint.user_id),
             subject=f'New comment {complaint.complaint_text}',
             message=comment.content
         )
+
+        logger.info('Comment created for user_id=%d, complaint_id=%d', user_id, comment_data.complaint_id)
         return await self._comment_repo.create(comment)
 
     async def get_comments_by_complaint(self, complaint_id: int) -> list[Comment]:
+        logger.debug('Fetching comments for complaint_id=%d', complaint_id)
         await self._complaint_service.get_by_id(complaint_id)
-        return await self._comment_repo.get_by_complaint_id(complaint_id)
+        comments = await self._comment_repo.get_by_complaint_id(complaint_id)
+        logger.info('Found %d comments for complaint_id=%d', len(comments), complaint_id)
+
+        return comments
 
     async def delete_comment(self, comment_id: int, user_id: int) -> bool:
+        logger.debug('Attempting to delete comment with comment_id=%d by user_id=%d', comment_id, user_id)
         comment = await self._comment_repo.get_by_id(comment_id)
+
         if not comment:
+            logger.error('Comment with comment_id=%d not found', comment_id)
             raise CommentNotFound(comment_id)
 
         if comment.user_id != user_id:
+            logger.warning('User_id=%d attempted to delete comment_id=%d that they do not own', user_id, comment_id)
             raise PermissionError("You do not have permission to delete this comment.")
 
-        return await self._comment_repo.delete(comment)
+        await self._comment_repo.delete(comment)
+        logger.info('Comment with comment_id=%d deleted by user_id=%d', comment_id, user_id)
+
+        return True
