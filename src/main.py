fba7d572bcf6f1240a1b1c9938e9b9a459732839
= <=>
2025-05-09 19:21:59 +0600

    refactor(main): delete if __name__ == __main__ construction

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
 from contextlib import asynccontextmanager
 
-import uvicorn
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 
-from src.core import settings, redis_client, database_helper
-from auth.routes import router as auth_router
+from src.core import redis_client, database_helper
+from src.auth.routes import router as auth_router
 from src.websocket.routes import router as websocket_router
-from users.routes import router as user_router
-from complaints.complaint_routes import router as complaint_router
-from comments.routes import router as comment_router
+from src.users.routes import router as user_router
+from src.complaints.complaint_routes import router as complaint_router
+from src.comments.routes import router as comment_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -29,13 +28,5 @@ app.include_router(user_router)
 app.include_router(complaint_router)
 app.include_router(comment_router)
 app.include_router(websocket_router)
-
-if __name__ == "__main__":
-    uvicorn.run(
-        'main:app',
-        port=settings.server_port,
-        host=settings.server_host,
-    )
-
 
 
