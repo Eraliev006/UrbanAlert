f54a461168b86bb4ec62633f52a9514d5e86673b
= <=>
2025-05-22 02:19:17 +0600

    delete integrity error exceptions

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -1,3 +1,3 @@
-from .exceptions import DatabaseError, IntegrityErrorException, ErrorResponse, BaseHTTPException, NotFoundException, AuthException
+from .exceptions import DatabaseError, ErrorResponse, BaseHTTPException, NotFoundException, AuthException
 from .base_repository import BaseRepository
 from .db_decorators import db_exception_handler
