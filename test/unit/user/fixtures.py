f54fd442ea8458ae758046080a33fba6dee4d87c
= <=>
2025-05-26 18:55:07 +0600

    test(user.fixtures): add fixtures to test user_service. - add mock_user_create,mock_user_update, mock_user all of this represents data of user but different class - user_service - return service with mocked repository and image_service

--- test/unit/user/fixtures.py
+++ test/unit/user/fixtures.py
@@ -1,19 +1,63 @@
+import datetime
+from unittest.mock import AsyncMock
+
 import pytest
+import pytest_asyncio
 
 from src import User
-from src.users import UserRepositories
+from src.users import UserRepositories, UserService, UserCreate, UserUpdate
 
 
-@pytest.fixture(scope='function')
+@pytest_asyncio.fixture(scope='function')
 def user_repository(session):
     return UserRepositories(session)
 
-@pytest.fixture(scope='function')
+@pytest_asyncio.fixture(scope='function')
 async def fake_user():
     return User(username="test", email="test@example.com", password="123")
 
 
-@pytest.fixture(scope='function')
+@pytest_asyncio.fixture(scope='function')
 async def user(user_repository):
     created = await user_repository.create(User(username="test", email="test@example.com", password="123"))
     return created
+
+@pytest_asyncio.fixture(scope='function')
+def mock_user_repository():
+    mock_repository = AsyncMock()
+    return mock_repository
+
+@pytest_asyncio.fixture(scope='function')
+def mock_image_service():
+    mock_image_service = AsyncMock()
+    return mock_image_service
+
+@pytest_asyncio.fixture(scope='function')
+async def user_service(mock_user_repository, mock_image_service):
+    return UserService(mock_user_repository,mock_image_service)
+
+@pytest_asyncio.fixture(scope='function')
+def mock_user():
+    return User(
+        id = 1,
+        username="test",
+        email="test@example.com",
+        password="123",
+        avatar_url = '',
+        created_at = datetime.date.today()
+    )
+
+@pytest_asyncio.fixture
+def mock_user_create():
+    return UserCreate(
+        username="test",
+        email="test@example.com",
+        password="123"
+    )
+
+@pytest.fixture
+def mock_user_update():
+    return UserUpdate(
+        username="updated_user",
+        email="updated@example.com"
+    )
