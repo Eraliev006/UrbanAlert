18f4da18e826fb8cfbb284a2ad8bb50e8b2fca1d
= <=>
2025-05-29 00:24:55 +0600

    test(complaint): Add and enhance fixtures for ComplaintService tests

--- test/unit/complaint/complaint_fixtures.py
+++ test/unit/complaint/complaint_fixtures.py
@@ -1,6 +1,11 @@
+import datetime
+from unittest.mock import AsyncMock
+
+import pytest
 import pytest_asyncio
 
-from src import Complaint, Comment
+from src import Complaint, Comment, ComplaintStatus
+from src.complaints import ComplaintService, ComplaintCreate, ComplaintUpdate
 from src.complaints.repositories import ComplaintRepositories
 from test.unit.user.fixtures import user
 
@@ -47,3 +52,54 @@ async def complaint_with_user_and_comments(complaint_repository, user, fake_comp
     await session.refresh(fake_complaint)
 
     return fake_complaint
+
+@pytest_asyncio.fixture(scope='function')
+async def mock_complaint_repository():
+    repository = AsyncMock()
+    return repository
+
+@pytest_asyncio.fixture(scope='function')
+async def mock_user_service():
+    service = AsyncMock()
+    return service
+
+
+@pytest_asyncio.fixture()
+async def complaint_service(mock_user_service, mock_image_service, mock_complaint_repository) -> ComplaintService:
+    return ComplaintService(
+        user_service=mock_user_service,
+        complaint_repo=mock_complaint_repository,
+        image_service=mock_image_service
+    )
+
+@pytest.fixture()
+def mock_complaint():
+    return Complaint(
+        id = 1,
+        complaint_text = 'some complaint',
+        category = 'road problem',
+        latitude = 0.000,
+        longitude = 1.000,
+        description = 'complaint description',
+        status = ComplaintStatus.PENDING,
+        image_url = '',
+        user_id = 1,
+        created_at = datetime.date.today(),
+        updated_at = datetime.date.today(),
+    )
+
+@pytest_asyncio.fixture()
+def mock_complaint_create():
+    return ComplaintCreate(
+        complaint_text='some complaint',
+        category='road problem',
+        latitude=0.000,
+        longitude=1.000,
+        description='complaint description',
+        status=ComplaintStatus.PENDING,
+    )
+@pytest_asyncio.fixture()
+def mock_complaint_update():
+    return ComplaintUpdate(
+        status = ComplaintStatus.APPROVED
+    )
